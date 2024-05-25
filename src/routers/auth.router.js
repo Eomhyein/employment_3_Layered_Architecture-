22bfd2bc1643ed1f8710b030b1cb5590b94f1ab6
hyein <mickey_8688@naver.com>
2024-05-26 01:11:39 +0900

    #4 signup : signup 05 URL update

--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -3,7 +3,7 @@
 // POST: /auth/sign-up
 import express from 'express';
 import { prisma } from '../utils/prisma.util.js'; // connectDb 연결
-import bcrypt from 'bcrypt'; // 1-5 bcrypt 리팩토링
+import bcrypt from 'bcrypt'; // 1-8 bcrypt 리팩토링
 
 const router = express.Router();
 // 1. 사용자 회원가입 API
@@ -46,26 +46,26 @@ router.post('/auth/sign-up', async (req, res, next) => {
       });
     }
 
-    // 1-7 비밀번호와 비밀번호 확인이 일치하지 않는 경우 - “입력 한 두 비밀번호가 일치하지 않습니다.”
+    // 1-5 비밀번호와 비밀번호 확인이 일치하지 않는 경우 - “입력 한 두 비밀번호가 일치하지 않습니다.”
     if (password !== passwordConfirm) {
       return res
         .status(400)
         .json({ message: '입력 한 두 비밀번호가 일치하지 않습니다.' });
     }
 
-    // 1-5 사용자 비밀번호를 암호화합니다
+    // 1-8 사용자 비밀번호를 암호화합니다
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // 1-3 Users 테이블에 이메일, 비밀번호, 이메일을 이용해 사용자를 생성한다.
+    // 1-6 Users 테이블에 이메일, 비밀번호, 이메일을 이용해 사용자를 생성한다.
     const users = await prisma.users.create({
       data: {
         email,
-        password: hashedPassword, // 1-5 암호화된 비밀번호를 저장한다
+        password: hashedPassword, // 1-8 암호화된 비밀번호를 저장한다
         name,
       },
     });
 
-    // 1-4 사용자 정보 반환(id, email, name, role, created_at, update_at)
+    // 1-7 사용자 정보 반환(id, email, name, role, created_at, update_at)
     return res.status(201).json({
       message: '회원가입이 완료되었습니다.',
       user: {
