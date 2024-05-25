56c3823fbd9c8ab6a496d72a2d01aeef8d3705c2
hyein <mickey_8688@naver.com>
2024-05-26 02:00:42 +0900

    #5 login : login 03

--- src/routers/users.router.js
+++ src/routers/users.router.js
@@ -9,7 +9,7 @@ import jwt from 'jsonwebtoken';
 const router = express.Router();
 
 // 2. 로그인 API
-router.post('/sign-in', async (req, res, next) => {
+router.post('/auth/sign-in', async (req, res, next) => {
   try {
     const { email, password } = req.body;
 
@@ -24,16 +24,12 @@ router.post('/sign-in', async (req, res, next) => {
     const user = await prisma.users.findFirst({ where: { email } });
 
     if (!user) {
-      return res
-        .status(401)
-        .json({ message: '이메일 형식이 올바르지 않습니다.' });
+      return res.status(401).json({ message: '이메일 형식이 올바르지 않습니다.' });
     }
     // 2-2 입력받은 사용자의 비밀번호와 데이터베이스에 저장된 비밀번호를 비교합니다.
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
-      return res
-        .status(401)
-        .json({ message: '인증 정보가 유효하지 않습니다.' });
+      return res.status(401).json({ message: '인증 정보가 유효하지 않습니다.' });
     }
     // 2-3 로그인에 성공하면, 사용자의 id를 바탕으로 토큰을 생성합니다.
     const token = jwt.sign(
