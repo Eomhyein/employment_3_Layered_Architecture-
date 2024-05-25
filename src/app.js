176bc253663aa89264e49af40d7d24836853e5f0
hyein <mickey_8688@naver.com>
2024-05-26 01:25:03 +0900

    #5 login : login 02

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import express from 'express';
 import { errorHandler } from './middlewares/error-handler.middleware.js';
 import { connectDb } from './utils/prisma.util.js'; // connectDb 연결
 import AuthRouter from './routers/auth.router.js'; // 1. 회원가입 라우터 연결
-// import UsersRouter from './routers/users.router.js'; // 2. 로그인 라우터 연결
+import UsersRouter from './routers/users.router.js'; // 2. 로그인 라우터 연결
 import cookieParser from 'cookie-parser'; // 쿠키
 import dotenv from 'dotenv';
 
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cookieParser()); // 쿠키
 
-app.use('/', AuthRouter); // 1. 회원가입 라우터 연결
+app.use('/', AuthRouter, UsersRouter); // 1. 회원가입, 로그인 라우터 연결
 
 app.use(errorHandler); // 에러 핸들링 미들웨어
 
