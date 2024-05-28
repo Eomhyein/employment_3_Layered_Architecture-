5f2c877e6a9aeb6be47a7e04712a8cc86b258016
hyein <mickey_8688@naver.com>
2024-05-28 14:55:14 +0900

    #11 /resumes/:id : /resumes/:id patch U 01

--- src/routers/resumes.router.js
+++ src/routers/resumes.router.js
@@ -166,4 +166,87 @@ router.get('/resumes/:id', accessTokenMiddle, async (req, res, next) => {
   }
 });
 
+// 4. 이력서 수정 API
+router.patch('/resumes/:id', accessTokenMiddle, async(req, res, next) => {
+  try {
+    const userId = req.user.id;
+    const {id} = req.params;
+    const{title, about_me} = req.body;
+
+    // 4-1 제목, 자기소개 둘 다 없는 경우
+    if(!title && !about_me) {
+      return res.status(404).json({message:'수정 할 정보를 입력해 주세요.'});
+    }
+    // 4-2 이력서 정보가 없는 경우
+    if(!id) {
+      return res.status(404).json({message:'이력서가 존재하지 않습니다.'});
+    }
+    // 4-3 현재 로그인 한 사용자가 작성한 이력서만 수정한다
+    // DB에서 이력서 조회 시 이력서 ID, 작성자 ID가 모두 일치해야 한다
+    const resume = await prisma.resume.findFirst({
+      where: {
+        user_id: userId,
+        id: +id
+      },
+      select: {
+        id: true,
+        user_id: true,
+        title: true,
+        about_me: true,
+        status: true,
+        created_at: true,
+        updated_at: true,
+        user: {
+          select: {
+            name: true
+          }
+        }
+      }
+    });
+    // 4-4 이력서 정보가 없는 경우
+    if(!resume) {
+      return res.status(404).json({message:'이력서가 존재하지 않습니다.'});
+    }
+    // 4-5 DB에서 이력서 정보를 수정한다
+    // 제목, 자기소개는 개별 수정이 가능하다
+    const resumeLogs = await prisma.resume.update({
+      where: {id: +id},
+      data: {
+        ...(title && {title}),
+        ...(about_me && {about_me}),
+      },
+      select: {
+        id: true,
+        user_id: true,
+        title: true,
+        about_me: true,
+        status: true,
+        created_at: true,
+        updated_at: true,
+        user: {
+          select :{
+            name: true
+          }
+        }
+      }
+    });
+    
+    // 4-6 수정 된 이력서id, 작성자 id, 제목, 자기소개, 지원상태, 생성일시, 수정일시 반환
+    const result = {
+      id : resumeLogs.id,
+      user_id: resumeLogs.user_id,
+      name: resumeLogs.user.name,
+      title: resumeLogs.title,
+      about_me: resumeLogs.about_me,
+      status: resumeLogs.status,
+      created_at: resumeLogs.created_at,
+      updated_at: resumeLogs.updated_at
+    };
+    return res.status(200).json({data: result});
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
 export default router;
